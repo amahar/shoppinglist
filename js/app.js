7a6f7a4238df4646ff23e9ebf8ea27703994642a
Asim <asim.mahar@gmail.com>
2015-08-21 16:40:43 -0400

    changes to layout

--- js/app.js
+++ js/app.js
@@ -5,22 +5,30 @@ $( "button" ).on( "click", function(){
 	var product = $('.productdescrip').val().trim();
 	var price = $('.price').val().trim();
 	var quantity = $('.quantity').val().trim();
-	var total = $('.total').val().trim();
+	//var total = $('.total').val().trim();
+	var totalCal = (quantity*price);
 
-	var arr = [product,price,quantity,total]
+	var arr = [product,price,quantity]
 
 	if (product.length == 0 ) {
-
-		$('#error').text("Please enter value");
-
-	} else if (price.length == 0) {
-
-		
-
-	}
-
+			$('.error1').text("Please enter product").show();
+			//$('.error1').show();
+		} else if (price.length == 0) {
+			$('.error2').text("Please enter price").show();
+		} else if (quantity.length == 0) {
+			$('.error3').text("Please enter quantity").show();
+	} else {
+		$('.append ul').append('<button class="removelist">Remove</button>');
+		$('.append ul').append('<li>' + product + '</li>').show();
+		$('.append ul').append('<li>' + price + '</li>').show();
+		$('.append ul').append('<li>' + quantity + '</li>').show();
+		$('.append ul').append('<li>' + totalCal + '</li>').show();
+		$('.error1, .error2, .error3').hide('');
+		}
 });
-
-
+$('.removelist').on("click",function(){
+	alert("this clicked");
+	//$(this).toggle();
+});
 });
 
